c708bb5ed38224d944cdec009bfdb0f1c485b910
Yifan Wang <yifan.wang0422@gmail.com>
2024-04-24 19:27:07 -0400

    implemented video recording

--- app.py
+++ app.py
@@ -1,67 +1,68 @@
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
-
-###### COMMANDS ######
-
-# Create a function to open camera and display it in the label_widget on app
-def open_camera():
-
-	# Capture the video frame by frame
-	_, frame = vid.read()
-
-	# Convert image from one color space to other
-	opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-
-	# Capture the latest frame and transform to image
-	captured_image = Image.fromarray(opencv_image)
-
-	# Convert captured image to photoimage
-	photo_image = ImageTk.PhotoImage(image=captured_image)
-
-	# Displaying photoimage in the label
-	video_label.photo_image = photo_image
-
-	# Configure image in the label
-	video_label.configure(image=photo_image)
-
-	# Repeat the same process after every 10 seconds
-	video_label.after(40, open_camera)
-
-# Starts video recording
-def start_recording():
-	print('video started recording')
-
-# Stops video recording
-def stop_recording():
-	print('video stopped recording')
-
-# Translates gloss to desire languauge
-def translate():
-	print('translate gloss')
-
-
-###### Application ######
+from datetime import datetime
 
 app = Tk()
 app.title('HandLingo')
-app.geometry('800x600')
 app.bind('<Escape>', lambda x: app.quit())
 
-# Creates label for video capture
-video_label = Label(app)
-video_label.pack(side='left', padx=5, pady=5, expand=True)
-vid = cv2.VideoCapture(0)
-width, height = 350, 500 # Declare the width and height in variables
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, width) # Set the width and height
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+# Shows video feed and records when button is pressed 
+recording = False
+frames_list = []
 
-open_camera()
+video_label = Label(app)
+video_label.pack(side='left', padx=5, pady=5)
+capture = cv2.VideoCapture(0)
+
+rand_counter = 0
+
+def update_frame():
+	global rand_counter
+	_, frame = capture.read()
+	opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+	captured_image = Image.fromarray(opencv_image)
+	if recording:
+		rand_counter += 1
+		frames_list.append(frame)
+	tk_img = ImageTk.PhotoImage(image=captured_image)
+	video_label.tk_img = tk_img
+	video_label.config(image=tk_img)
+	app.after(40, update_frame)
+
+def stop_rec():
+	global recording
+	recording = False
+
+	start_button.config(state="normal")
+	stop_button.config(state="disabled")
+
+	print('counter:', rand_counter)
+	print('recorded:', len(frames_list))
+
+	now = datetime.now()
+	dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+	fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+	video_writer = cv2.VideoWriter((dt_string+'.mp4'), fourcc, 11.0, (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+	for frame in frames_list:
+		video_writer.write(frame)
+	video_writer.release()
+
+def start_rec():
+    global recording
+    recording = True
+
+    start_button.config(state="disabled")
+    stop_button.config(state="normal")
+
+def closeWindow():
+    stop_rec()
+    app.destroy()
 
 # Creates buttons for video recording
-start_button = Button(app, text='Start Recording', command=start_recording)
+start_button = Button(app, text='Start Recording', command=start_rec)
 start_button.pack()
-stop_button = Button(app, text='Stop Recording', command=stop_recording)
+stop_button = Button(app, text='Stop Recording', command=stop_rec, state="disabled")
 stop_button.pack()
 
 # Creates language selector
@@ -81,13 +82,20 @@ language_selected.set('Unselected')
 languages_dropdown = OptionMenu(app, language_selected, *languages_list)
 languages_dropdown.pack()
 
+# Translates gloss to desire languauge
+def translate():
+	print('translate to this language:')
+	print(language_selected.get())
+
 # Creates translate button
 translate_button = Button(app, text='Translate', command=translate)
 translate_button.pack()
 
 # Creates gloss and translation log
 log = Message(app, bd=2, bg='gray')
-log.pack()
+log.pack(expand=True, fill="both")
+
+update_frame()
 
 # Create an infinite loop for displaying app on screen
 app.mainloop()
