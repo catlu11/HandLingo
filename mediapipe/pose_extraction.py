9bcc524243dd783f2ce866ab1b43973ae41d50a3
Yifan Wang <yifan.wang0422@gmail.com>
2024-05-06 18:46:29 -0400

    added pose normalization

--- mediapipe/pose_extraction.py
+++ mediapipe/pose_extraction.py
@@ -1,24 +1,34 @@
 import cv2
 import mediapipe as mp
 import csv
+import math
 
-def write_landmarks_to_csv(landmarks, frame_number, csv_data, landmark_type, empty=False):
+## POSE EXTRACTION
+
+def write_landmarks_to_csv(landmarks, frame_number, csv_data, landmark_type, norm_landmarks, empty=False):
     for idx, landmark in enumerate(landmarks):
         if landmark_type == "POSE":
-            if 0 < idx <= 24:
-                csv_data.append([frame_number, mp_holistic.PoseLandmark(idx).name, landmark.x, landmark.y, landmark.z])
+            if (0 <= idx <= 14) or (idx == 23) or (idx ==24):
+                if idx == 0:
+                    norm_landmarks["nose_x"] = landmark.x
+                    norm_landmarks["nose_y"] = landmark.y
+                if idx == 3:
+                    norm_landmarks["left_eye_outer_x"] = landmark.x
+                if idx == 6:
+                    norm_landmarks["right_eye_outer_x"] = landmark.x
+                csv_data.append([frame_number, mp_holistic.PoseLandmark(idx).name, landmark.x, landmark.y])
         else:
             if empty:
-                csv_data.append([frame_number, landmark_type + "_" + mp_holistic.HandLandmark(idx).name, 0, 0, 0])
+                csv_data.append([frame_number, landmark_type + "HAND_" + mp_holistic.HandLandmark(idx).name, None, None])
             else:
-                csv_data.append([frame_number, landmark_type + "_" + mp_holistic.HandLandmark(idx).name, landmark.x, landmark.y, landmark.z])
+                csv_data.append([frame_number, landmark_type + "HAND_" + mp_holistic.HandLandmark(idx).name, landmark.x, landmark.y])
 
 video_path = "orange.mp4"
-output_csv = "output.csv"
+output_csv = "extracted_features.csv"
 
 # Initialize MediaPipe Pose and Drawing utilities
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 holistic = mp_holistic.Holistic()
 
@@ -27,33 +37,31 @@ cap = cv2.VideoCapture(video_path)
 
 frame_number = 0
 csv_data = []
+bounding_box = {}
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Convert the frame to RGB
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Process the frame with MediaPipe Pose
     result = holistic.process(frame_rgb)
 
-    # Draw the pose and hand landmarks on the frame
+    norm_landmarks = {}
 
-    if (result.left_hand_landmarks or result.right_hand_landmarks):
+    if (result.pose_landmarks):
+        write_landmarks_to_csv(result.pose_landmarks.landmark, frame_number, csv_data, "POSE", norm_landmarks)
+
         if result.left_hand_landmarks:
-            write_landmarks_to_csv(result.left_hand_landmarks.landmark, frame_number, csv_data, "LEFT") 
+            write_landmarks_to_csv(result.left_hand_landmarks.landmark, frame_number, csv_data, "LEFT", norm_landmarks) 
         else:
-            write_landmarks_to_csv(mp_holistic.HandLandmark, frame_number, csv_data, "LEFT", empty=True)
+            write_landmarks_to_csv(mp_holistic.HandLandmark, frame_number, csv_data, "LEFT", norm_landmarks, empty=True)
 
         if result.right_hand_landmarks:
-            write_landmarks_to_csv(result.right_hand_landmarks.landmark, frame_number, csv_data, "RIGHT") 
+            write_landmarks_to_csv(result.right_hand_landmarks.landmark, frame_number, csv_data, "RIGHT", norm_landmarks) 
         else:
-            write_landmarks_to_csv(mp_holistic.HandLandmark, frame_number, csv_data, "RIGHT", empty=True)
-
-        if result.pose_landmarks:
-            write_landmarks_to_csv(result.pose_landmarks.landmark, frame_number, csv_data, "POSE")
+            write_landmarks_to_csv(mp_holistic.HandLandmark, frame_number, csv_data, "RIGHT", norm_landmarks, empty=True)
 
     # frame_rgb = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
 
@@ -72,15 +80,43 @@ while cap.isOpened():
 
     # # Display the frame
     # cv2.imshow('MediaPipe', frame_rgb)
+        nose_x = norm_landmarks["nose_x"]
+        nose_y = norm_landmarks["nose_y"]
+        left_eye_outer_x = norm_landmarks["left_eye_outer_x"]
+        right_eye_outer_x = norm_landmarks["right_eye_outer_x"]
+
+        head_unit = abs(right_eye_outer_x - left_eye_outer_x)
+        height_unit = head_unit * (frame.shape[1] / frame.shape[0])
+        lower_x = nose_x - (4 * head_unit)
+        upper_x = nose_x + (4 * head_unit)
+        lower_y = nose_y - (1.5 * height_unit) 
+        upper_y = nose_y + (7 * height_unit)
+        bounding_box[frame_number] = (lower_x, upper_x, lower_y, upper_y)
 
     frame_number += 1
 
 cap.release()
 cv2.destroyAllWindows()
 
+## POSE NORMALIZATION
+
+def pose_normalization(old_data, bounds):
+    normalized = []
+    for feature in old_data:
+        frame_num, name, x, y = feature
+        if x is None and y is None:
+            new_x = 0
+            new_y = 0
+        else:
+            new_x = (x - bounds[frame_num][0]) / (bounds[frame_num][1] - bounds[frame_num][0])
+            new_y = (y - bounds[frame_num][2]) / (bounds[frame_num][3] - bounds[frame_num][2])
+        normalized.append([frame_num, name, new_x, new_y])
+    return normalized
+
+normalized_csv_data = pose_normalization(csv_data, bounding_box)
 
 # Save the CSV data to a file
 with open(output_csv, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['frame_number', 'landmark', 'x', 'y', 'z'])
-    csv_writer.writerows(csv_data)
+    csv_writer.writerow(['frame_number', 'landmark', 'x', 'y'])
+    csv_writer.writerows(normalized_csv_data)
