3c0bc81600a7ab1721978a9a2c9a4a540d27b7a4
Catherine Lu <czlu@mit.edu>
2024-05-08 20:32:30 -0400

    Add TTS function

--- translation.py
+++ translation.py
@@ -1,9 +1,18 @@
 from googletrans import Translator
+from gtts import gTTS
+from pygame import mixer
+import os
 
 class Translation:
 	def __init__(self):
 		self.translator = Translator()
+		mixer.init()
 
 	def get_translation(self, text, dest="zh-CN"):
 		return self.translator.translate(text, dest=dest).text
 
+	def play_tts(self, text="Something went wrong", lang="en"):
+		mp3 = gTTS(text=text, lang=lang, slow=False)
+		mp3.save("temp.mp3")
+		mixer.music.load("temp.mp3")
+		mixer.music.play()
